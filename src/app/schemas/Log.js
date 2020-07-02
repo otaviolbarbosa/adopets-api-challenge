00cab4ec408a88cc6c3c14bd2c4ffb3329942fd8
Otavio Barbosa <otavio.leite@bairesdev.com>
2020-07-01 23:31:31 -0300

    uuid fix

--- src/app/schemas/Log.js
+++ src/app/schemas/Log.js
@@ -5,26 +5,7 @@ const Log = new Schema(
   {
     _id: {
       type: String,
-      default: uuid({
-        random: [
-          0x10,
-          0x91,
-          0x56,
-          0xbe,
-          0xc4,
-          0xfb,
-          0xc1,
-          0xea,
-          0x71,
-          0xb4,
-          0xef,
-          0xe1,
-          0x67,
-          0x1c,
-          0x58,
-          0x36,
-        ],
-      }),
+      default: uuid(),
     },
     user_id: {
       type: String,
